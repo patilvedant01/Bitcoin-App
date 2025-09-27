0d6b3956a94b12f2caa873fac4d0cc6beee8cd44
vedantCarousell <vedant.patil@thecarousell.com>
2025-09-27 11:41:08 +0530

    Initial Commit

---
+++ BitcoinApp/BitcoinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinAppApp.swift
+//  BitcoinApp
+//
+//  Created by Vedant Patil on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitcoinApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BitcoinApp
+//
+//  Created by Vedant Patil on 27/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
